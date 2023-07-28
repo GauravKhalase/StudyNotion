6bbd218f0a7d5d5932953fd48730762763ddf6fa
GauravKhalase <gauravkhalase@gmail.com>
2023-07-28 17:56:08 +0530

    final commit

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000" || "https://study-notion-nu.vercel.app/",
+    origin: "http://localhost:3000" || "https://study-notion-nu.vercel.app",
     credentials: true,
   })
 );
